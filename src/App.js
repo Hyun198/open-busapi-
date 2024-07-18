3835e77e6037f639c815728985f859a521475d91
Hyun <hwn123h@naver.com>
2024-07-19 01:46:23 +0900

    버스 실시간 정보 조회

--- src/App.js
+++ src/App.js
@@ -78,6 +78,37 @@ function App() {
     }
   };
 
+  const fetchBusLocation = async () => {
+    try {
+      const url = `http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList?serviceKey=${serviceKey}&routeId=232000009`
+      const response = await axios.get(url, {
+        headers: {
+          'Content-Type': 'text/xml; charset=utf-8'
+        },
+        responseType: 'text'
+      });
+
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(response.data, 'text/xml');
+      const busLocationList = xmlDoc.getElementsByTagName('busLocationList');
+      if (busLocationList.length === 0) {
+        throw new Error('No bus location info item found');
+      }
+
+      const busLocation = busLocationList[0];
+      const busLoc = {
+        endBus: busLocation.getElementsByTagName('endBus')[0].textContent,
+        lowPlate: busLocation.getElementsByTagName('lowPlate')[0].textContent,
+        plateNo: busLocation.getElementsByTagName('plateNo')[0].textContent,
+      }
+      console.log('busLoc', busLoc);
+
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
 
   useEffect(() => {
     const keyword = '52'; // 사용자 입력값으로 대체 가능
@@ -91,6 +122,7 @@ function App() {
     };
 
     getBusCode();
+    fetchBusLocation();
   }, []);
 
 
